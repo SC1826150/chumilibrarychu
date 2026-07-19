2b65a8c76b515ef44d1e6fe10bf4f0f408d70c81
ChumiChu <littlewifevai@gmail.com>
2026-07-19 10:53:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-body{
-    font-family: Arial, sans-serif;
-    background:#f9f3ff;
-    text-align:center;
-    padding:50px;
-}
-
-h1{
-    color:#b030b0;
-}
+console.log("Welcome to Chumi's Library!");
