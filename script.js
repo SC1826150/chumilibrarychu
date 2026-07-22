6b80662638c0bc6f2ad3f9e4e0a5804f31cbe0ef
ShenmaChu <littlewifevai@gmail.com>
2026-07-22 12:03:03 +0800

    Implement showTab function for tab navigation
    Add showTab function to manage tab visibility

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("Welcome to Chu's Personal Library!");
+function showTab(tab){
+
+    document.querySelectorAll(".tab-content")
+    .forEach(content=>content.classList.remove("active"));
+
+    document.querySelectorAll(".tab")
+    .forEach(button=>button.classList.remove("active"));
+
+    document.getElementById(tab)
+    .classList.add("active");
+
+    event.target.classList.add("active");
+
+}
